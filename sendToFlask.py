79ea102d467ba0093fe58fbadf1cd3f7005ce580
HJ <145420858+hyejung0906@users.noreply.github.com>
2024-03-25 22:43:47 +0900

    Add files via upload

--- sendToFlask.py
+++ sendToFlask.py
@@ -1,7 +1,7 @@
 # sendToFlask.py
 
 from flask import Flask, render_template, request
-import base64
+import json
 from PIL import Image
 from io import BytesIO
 import requests
@@ -27,17 +27,19 @@ def notice():
 def drawing_page():
     return render_template('serviceDrawBoard.html')
 
+@app.route('/drawing/popup')
+def identifier_call():
+    return render_template('idPopUp.html')
+
 @app.route('/drawing/analyze', methods=['POST'])
 def analyze():
     data = request.form.to_dict()
     identifier = data.get('identifier')
 
-    # 이미지 base64 디코딩
-    image_data = data.get('imageData')
-    image_data = base64.b64decode(image_data.split(',')[1])
-    image = Image.open(BytesIO(image_data))
+    # 이미지 로드
+    image_path = 'C:/Users/jico0/Downloads/PaintJS[EXPORT].png'  # 로컬 이미지 파일 경로
+    image = Image.open(image_path)
     image = image.resize((256, 256))
-    print(image_data)
 
     # 이미지 전처리
     image = np.array(image)
@@ -62,30 +64,17 @@ def analyze():
     # 생성된 이미지를 Base64 문자열로 인코딩
     buffered = BytesIO()
     generated_image.save(buffered, format="PNG")
+    generated_image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-    # Bing Visual Search API 호출
-    headers = {'Ocp-Apim-Subscription-Key': '84900ec94e08423b9468f053da33eff9', 'Content-Type': 'application/octet-stream'}
-    params = {'mkt': 'ko-KR'}
-    response = requests.post(
-        "https://api.bing.microsoft.com/v7.0/images/visualsearch",
-        headers = headers,
-        params = params,
-        data = buffered.getvalue()
-     )
+    return render_template('serviceModelResult.html', generated_image_base64=generated_image_base64)
 
-    # 검색 결과 처리
-    search_results = response.json()
+@app.route('/render')
+def model_result():
+    return render_template('serviceModelResult.html')
 
-    # 결과 페이지로 이동 및 검색 결과 데이터 전달
-    return render_template('serviceResult.html', result=result, search_results=search_results)
-
-@app.route('/result', methods=['GET', 'POST'])
-def result():
-    if request.method == 'POST':
-        # 검색 결과를 처리하는 로직 작성
-        return "Search results received and processed!"
-    else:
-        return "Invalid request method for this endpoint."
+@app.route('/search')
+def search_result():
+    return render_template('serviceSearchResult.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
